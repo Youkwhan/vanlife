1c5382dbb43f78873fafd2ae192f019a921611fa
Youkwhan <Youkwhan@gmail.com>
2023-06-08 22:19:05 -0400

    Rendering Van elements

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,9 +1,26 @@
 import { useEffect, useState } from "react"
+import "./Vans.css"
 
 function Vans() {
 	const [vans, setVans] = useState([])
 	console.log(vans)
 
+	// Array of JSX van elements, to render per grid cell
+	const vanElements = vans.map((van) => (
+		<div key={van.id} className="van-tile">
+			<img src={van.imageUrl} />
+			<div className="van-info">
+				<h3>{van.name}</h3>
+				<p>
+					${van.price}
+					<span>/day</span>
+				</p>
+			</div>
+			<i className={`van-type ${van.type} selected`}>{van.type}</i>
+		</div>
+	))
+
+	// fetch vans data
 	useEffect(() => {
 		const fetchVanData = async () => {
 			try {
@@ -18,7 +35,12 @@ function Vans() {
 		fetchVanData()
 	}, [])
 
-	return <h1>Vans page goes here 🚐</h1>
+	return (
+		<div className="van-list-container">
+			<h1>Explore our van options</h1>
+			<div className="van-list">{vanElements}</div>
+		</div>
+	)
 }
 
 export default Vans
