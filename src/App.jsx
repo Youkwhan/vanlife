169507443c08b37146a92d84212fec2f8aeb1098
Youkwhan <Youkwhan@gmail.com>
2023-06-10 17:46:01 -0400

    Refactor files and Add Host Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css"
-import { Routes, Route, Link } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from "./pages/Vans"
-import VanDetail from "./pages/VanDetail"
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
+
 import Layout from "./components/Layout"
 
 import "./server"
@@ -12,11 +16,14 @@ function App() {
 	return (
 		<>
 			<Routes>
-				<Route  element={<Layout />}>
+				<Route element={<Layout />}>
 					<Route path="/" element={<Home />} />
 					<Route path="/about" element={<About />} />
 					<Route path="/vans" element={<Vans />} />
 					<Route path="/vans/:id" element={<VanDetail />} />
+					<Route path="/host" element={<Dashboard />} />
+					<Route path="/host/income" element={<Income />} />
+					<Route path="/host/reviews" element={<Reviews />} />
 				</Route>
 			</Routes>
 		</>
---
+++ src/pages/Host/Dashboard.jsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+	return <h1>Reviews page goes here</h1>
+}
+
+export default Dashboard
