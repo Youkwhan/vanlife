699fff4b2f7f49c4371f0627f0fe77eeff15cb02
Youkwhan <Youkwhan@gmail.com>
2023-06-15 20:46:53 -0400

    Add: conditionally render  selected button css

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -51,28 +51,36 @@ function Vans() {
 			<div className="van-list-filter-buttons">
 				<button
 					onClick={() => setSearchParams({ type: "simple" })}
-					className="van-type simple"
+					className={`van-type simple ${
+						typeFilter === "simple" ? "selected" : ""
+					}`}
 				>
 					Simple
 				</button>
 				<button
 					onClick={() => setSearchParams({ type: "luxury" })}
-					className="van-type luxury"
+					className={`van-type luxury ${
+						typeFilter === "luxury" ? "selected" : ""
+					}`}
 				>
 					Luxury
 				</button>
 				<button
 					onClick={() => setSearchParams({ type: "rugged" })}
-					className="van-type rugged"
+					className={`van-type rugged ${
+						typeFilter === "rugged" ? "selected" : ""
+					}`}
 				>
 					Rugged
 				</button>
-				<button
-					onClick={() => setSearchParams({})}
-					className="van-type clear-filters"
-				>
-					Clear filter
-				</button>
+				{typeFilter && (
+					<button
+						onClick={() => setSearchParams({})}
+						className="van-type clear-filters"
+					>
+						Clear filter
+					</button>
+				)}
 			</div>
 			<div className="van-list">{vanElements}</div>
 		</div>
