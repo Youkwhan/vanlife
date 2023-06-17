9a0c70dc007fc1611f0b84acad9d2939a55e8efa
Youkwhan <Youkwhan@gmail.com>
2023-06-17 17:35:05 -0400

    Add: Login page
    - To introduce protected routes for our Host route, which should be personalized.

--- src/server.js
+++ src/server.js
@@ -80,8 +80,8 @@ createServer({
 		//this.timing = 2000 // delay to test loading
 
 		this.get("/vans", (schema, request) => {
-			return new Response(400, {}, { error: "Error fetching data" }) // Test error
-			// return schema.vans.all()
+			// return new Response(400, {}, { error: "Error fetching data" }) // Test error
+			return schema.vans.all()
 		})
 
 		this.get("/vans/:id", (schema, request) => {
