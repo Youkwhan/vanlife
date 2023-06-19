78c361958be35c53dc4f3c1ff898662f279ac030
Youkwhan <Youkwhan@gmail.com>
2023-06-19 14:52:00 -0400

    Start: protected routes with loaders

--- src/App.jsx
+++ src/App.jsx
@@ -39,16 +39,70 @@ const router = createBrowserRouter(
 				errorElement={<Error />}
 			/>
 			<Route path="vans/:id" element={<VanDetail />} />
-
+			{/* Host route should be protected per login user */}
 			<Route path="/host" element={<HostLayout />}>
-				<Route index element={<Dashboard />} />
-				<Route path="income" element={<Income />} />
-				<Route path="reviews" element={<Reviews />} />
-				<Route path="vans" element={<HostVans />} />
-				<Route path="vans/:id" element={<HostVanDetail />}>
-					<Route index element={<HostVanInfo />} />
-					<Route path="pricing" element={<HostVanPricing />} />
-					<Route path="photos" element={<HostVanPhotos />} />
+				<Route
+					index
+					element={
+						<Dashboard
+							loader={async () => {
+								return null
+							}}
+						/>
+					}
+				/>
+				<Route
+					path="income"
+					element={<Income />}
+					loader={async () => {
+						return null
+					}}
+				/>
+				<Route
+					path="reviews"
+					element={<Reviews />}
+					loader={async () => {
+						return null
+					}}
+				/>
+				<Route
+					path="vans"
+					element={<HostVans />}
+					loader={async () => {
+						return null
+					}}
+				/>
+				<Route
+					path="vans/:id"
+					element={
+						<HostVanDetail
+							loader={async () => {
+								return null
+							}}
+						/>
+					}
+				>
+					<Route
+						index
+						element={<HostVanInfo />}
+						loader={async () => {
+							return null
+						}}
+					/>
+					<Route
+						path="pricing"
+						element={<HostVanPricing />}
+						loader={async () => {
+							return null
+						}}
+					/>
+					<Route
+						path="photos"
+						element={<HostVanPhotos />}
+						loader={async () => {
+							return null
+						}}
+					/>
 				</Route>
 			</Route>
 			<Route path="*" element={<NotFound />} />
