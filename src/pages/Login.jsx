e7766bf697995aa4d8df342a22047071f256c4cd
Youkwhan <Youkwhan@gmail.com>
2023-06-19 21:59:45 -0400

    Use the formData in action to log-in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,16 +12,13 @@ export async function action({ request }) {
 	const formData = await request.formData()
 	const email = formData.get("email")
 	const password = formData.get("password")
-	console.log(email, password)
-	
+	const data = await loginUser({ email, password })
+	console.log(data)
+
 	return null
 }
 
 export default function Login() {
-	const [loginFormData, setLoginFormData] = useState({
-		email: "",
-		password: "",
-	})
 	const [status, setStatus] = useState("idle")
 	const [error, setError] = useState(null)
 	const navigate = useNavigate()
@@ -40,14 +37,6 @@ export default function Login() {
 			.finally(() => setStatus("idle"))
 	}
 
-	function handleChange(e) {
-		const { name, value } = e.target
-		setLoginFormData((prev) => ({
-			...prev,
-			[name]: value,
-		}))
-	}
-
 	return (
 		<div className="login-container">
 			<h1>Sign in to your account</h1>
@@ -55,20 +44,8 @@ export default function Login() {
 			{error && <h3 className="red">{error.message}</h3>}
 
 			<Form method="post" className="login-form">
-				<input
-					name="email"
-					onChange={handleChange}
-					type="email"
-					placeholder="Email address"
-					value={loginFormData.email}
-				/>
-				<input
-					name="password"
-					onChange={handleChange}
-					type="password"
-					placeholder="Password"
-					value={loginFormData.password}
-				/>
+				<input name="email" type="email" placeholder="Email address" />
+				<input name="password" type="password" placeholder="Password" />
 				<button disabled={status === "submitting"}>
 					{status === "submitting" ? "Logging in..." : "Log in"}
 				</button>
