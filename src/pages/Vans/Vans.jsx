648355ea1933c10f7998a1ae600f598e9fc1d63c
Youkwhan <Youkwhan@gmail.com>
2023-06-21 17:38:01 -0400

    Add: Await
    - Await allows us to surround the code that we would be waiting for when the component first renders and it will conditionally render only after the data has finished loading from the defered promise.
    - Await is a custom component so, we can add js functions and jsx
    - In this case: Await compoonent will only call the child function after it has finished resolving our promised data. Then we can again be sure that our useLoadedData() exists when rendering.

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,4 +1,10 @@
-import { Link, useSearchParams, useLoaderData, defer } from "react-router-dom"
+import {
+	Link,
+	useSearchParams,
+	useLoaderData,
+	defer,
+	Await,
+} from "react-router-dom"
 import "./Vans.css"
 import { getVans } from "../../utils/api"
 
@@ -8,76 +14,98 @@ export function loader() {
 
 function Vans() {
 	const [searchParams, setSearchParams] = useSearchParams()
-
-	// Fetch vans data with loader function
-	const vans = useLoaderData()
+	// deferred promise of vans data in loader function
+	const dataPromise = useLoaderData()
 
 	// GET filtered search params
 	const typeFilter = searchParams.get("type")
 
-	const displayedVans = typeFilter
-		? vans.filter((van) => van.type === typeFilter)
-		: vans
+	function handleFilterChange(key, value) {
+		setSearchParams((prevParams) => {
+			if (value === null) {
+				prevParams.delete(key)
+			} else {
+				prevParams.set(key, value)
+			}
+			return prevParams
+		})
+	}
 
-	// Array of JSX van elements, to render per grid cell
-	const vanElements = displayedVans.map((van) => (
-		<div key={van.id} className="van-tile">
-			{/* wrap in a Link so the whole card can be selected and redirected to van.id details page*/}
-			<Link
-				to={van.id}
-				state={{ search: `?${searchParams.toString()}`, type: typeFilter }}
-			>
-				<img src={van.imageUrl} />
-				<div className="van-info">
-					<h3>{van.name}</h3>
-					<p>
-						${van.price}
-						<span>/day</span>
-					</p>
+	// Await component children
+	function renderVanElements(vans) {
+		const displayedVans = typeFilter
+			? vans.filter((van) => van.type === typeFilter)
+			: vans
+
+		// Array of JSX van elements, to render per grid cell
+		const vanElements = displayedVans.map((van) => (
+			<div key={van.id} className="van-tile">
+				{/* wrap in a Link so the whole card can be selected and redirected to van.id details page*/}
+				<Link
+					to={van.id}
+					state={{
+						search: `?${searchParams.toString()}`,
+						type: typeFilter,
+					}}
+				>
+					<img src={van.imageUrl} />
+					<div className="van-info">
+						<h3>{van.name}</h3>
+						<p>
+							${van.price}
+							<span>/day</span>
+						</p>
+					</div>
+					<i className={`van-type ${van.type} selected`}>{van.type}</i>
+				</Link>
+			</div>
+		))
+
+		return (
+			<>
+				<div className="van-list-filter-buttons">
+					<button
+						onClick={() => handleFilterChange("type", "simple")}
+						className={`van-type simple ${
+							typeFilter === "simple" ? "selected" : ""
+						}`}
+					>
+						Simple
+					</button>
+					<button
+						onClick={() => handleFilterChange("type", "luxury")}
+						className={`van-type luxury ${
+							typeFilter === "luxury" ? "selected" : ""
+						}`}
+					>
+						Luxury
+					</button>
+					<button
+						onClick={() => handleFilterChange("type", "rugged")}
+						className={`van-type rugged ${
+							typeFilter === "rugged" ? "selected" : ""
+						}`}
+					>
+						Rugged
+					</button>
+					{typeFilter && (
+						<button
+							onClick={() => handleFilterChange("type", null)}
+							className="van-type clear-filters"
+						>
+							Clear filter
+						</button>
+					)}
 				</div>
-				<i className={`van-type ${van.type} selected`}>{van.type}</i>
-			</Link>
-		</div>
-	))
+				<div className="van-list">{vanElements}</div>
+			</>
+		)
+	}
 
 	return (
 		<div className="van-list-container">
 			<h1>Explore our van options</h1>
-			<div className="van-list-filter-buttons">
-				<button
-					onClick={() => setSearchParams({ type: "simple" })}
-					className={`van-type simple ${
-						typeFilter === "simple" ? "selected" : ""
-					}`}
-				>
-					Simple
-				</button>
-				<button
-					onClick={() => setSearchParams({ type: "luxury" })}
-					className={`van-type luxury ${
-						typeFilter === "luxury" ? "selected" : ""
-					}`}
-				>
-					Luxury
-				</button>
-				<button
-					onClick={() => setSearchParams({ type: "rugged" })}
-					className={`van-type rugged ${
-						typeFilter === "rugged" ? "selected" : ""
-					}`}
-				>
-					Rugged
-				</button>
-				{typeFilter && (
-					<button
-						onClick={() => setSearchParams({})}
-						className="van-type clear-filters"
-					>
-						Clear filter
-					</button>
-				)}
-			</div>
-			<div className="van-list">{vanElements}</div>
+			<Await resolve={dataPromise.vans}>{renderVanElements}</Await>
 		</div>
 	)
 }
